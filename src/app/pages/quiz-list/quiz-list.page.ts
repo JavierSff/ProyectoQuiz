fd8a9c41359c7aa42e9104361c5c1d8cd8210f1a
Javier <166989981+JavierSff@users.noreply.github.com>
2025-04-06 18:32:56 +0200

    Quiz delete working
    Now we can delete the quizes from the quiz list

--- src/app/pages/quiz-list/quiz-list.page.ts
+++ src/app/pages/quiz-list/quiz-list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService, Quiz } from 'src/app/services/quiz-service.service';
 import { AuthenticationService } from 'src/app/authentication.service';
-import { NavController } from '@ionic/angular';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-quiz-list',
@@ -12,11 +12,14 @@ import { NavController } from '@ionic/angular';
 export class QuizListPage implements OnInit {
   quizzes: Quiz[] = [];
   userId: string = '';
+  deleteMode = false;
+  selectedQuizzes: string[] = [];
 
   constructor(
     private quizService: QuizService,
     private authService: AuthenticationService,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private toastCtrl: ToastController
   ) {}
 
   ngOnInit() {
@@ -29,10 +32,52 @@ export class QuizListPage implements OnInit {
   }
 
   takeQuiz(id: string) {
-    this.navCtrl.navigateForward(['/quiz-runner', id]);
+    // If delete mode is active, don’t navigate on item click.
+    if (!this.deleteMode) {
+      this.navCtrl.navigateForward(['/quiz-runner', id]);
+    }
   }
 
   createQuiz() {
     this.navCtrl.navigateForward(['/quiz-creator']);
+  }
+
+  toggleDeleteMode() {
+    this.deleteMode = !this.deleteMode;
+    // Clear any selections when toggling out of delete mode.
+    if (!this.deleteMode) {
+      this.selectedQuizzes = [];
+    }
+  }
+
+  onSelectQuiz(event: any, id: string) {
+    if (event.detail.checked) {
+      if (!this.selectedQuizzes.includes(id)) {
+        this.selectedQuizzes.push(id);
+      }
+    } else {
+      this.selectedQuizzes = this.selectedQuizzes.filter(qid => qid !== id);
+    }
+  }
+
+  async deleteSelectedQuizzes() {
+    const deletePromises = this.selectedQuizzes.map(id => this.quizService.deleteQuiz(id));
+    try {
+      await Promise.all(deletePromises);
+      const toast = await this.toastCtrl.create({
+        message: 'Successfully deleted selected quizzes!',
+        duration: 2000,
+      });
+      toast.present();
+      // Clear selections and turn off delete mode.
+      this.selectedQuizzes = [];
+      this.deleteMode = false;
+    } catch (error) {
+      const toast = await this.toastCtrl.create({
+        message: 'Error deleting quizzes',
+        duration: 2000,
+      });
+      toast.present();
+    }
   }
 }

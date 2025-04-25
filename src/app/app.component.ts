fe62c59a12126ed5920eefcacb150a1b99a0899d
Javier <166989981+JavierSff@users.noreply.github.com>
2025-04-25 13:44:37 +0200

    added cancel button on signup and login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router, NavigationEnd } from '@angular/router';
+import { MenuController } from '@ionic/angular';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +11,35 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,
+    private router: Router,
+    private menuCtrl: MenuController
+  ) {
+    this.initializeApp();
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.handleMenuVisibility();
+    });
+  }
+
+  handleMenuVisibility() {
+    const hiddenRoutes = [
+      '/landing',
+      '/splash-page',
+      '/login',
+      '/signup',
+      '/forgot-password'
+    ];
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const currentUrl = event.urlAfterRedirects;
+        const shouldDisable = hiddenRoutes.includes(currentUrl);
+        this.menuCtrl.enable(!shouldDisable);
+      });
+  }
 }
+

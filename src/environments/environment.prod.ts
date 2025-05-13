32105a9ed95fc137fc4fc40bbcc1f155a830b2fb
Javier <166989981+JavierSff@users.noreply.github.com>
2025-05-13 19:41:08 +0200

    apk generated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {  
+    apiKey: "AIzaSyDZUvNqyUh16OMi5eE6lwT1hayfPk0a5D4",
+    authDomain: "miapp-92693.firebaseapp.com",
+    projectId: "miapp-92693",
+    storageBucket: "miapp-92693.firebasestorage.app",
+    messagingSenderId: "139009552761",
+    appId: "1:139009552761:web:b6135c04df4dd6d6403fa8"
+  },
+  weatherApi: {
+    API_KEY: 'd4e4dc67c4c8bc8649cc849525c59371',
+    API_URL: 'https://api.openweathermap.org/data/2.5'
+  }
 };
